e93aa5155f38bfd8be16bf2198bf96618d36eba1
RonLamp <lamp@rc12.net>
2023-04-29 10:07:58 -0300

    fifth commit eloquent 1/n

--- app/Models/Fornecedor.php
+++ app/Models/Fornecedor.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Fornecedor extends Model
 {
     use HasFactory;
+    // quando se define assim o nome da tabela, este nome se sobrepões
+    // ao nome padrão dado pelo laravel. 
+    // Se a classe fosse : SpecialFornecedor
+    // o nome padrão da tabela seria:
+    // Special_Fornecedor
+    // special_fornecedor
+    // special_fornecedors
+    // mas que passa a ser fornecedors
+    protected $table = 'fornecedors';
+    protected $fillable = ['nome','site','uf','email'];
 }
